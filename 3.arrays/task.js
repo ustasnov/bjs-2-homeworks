f6a4b5806441ad4c6c0abfe6133e5974c5e03cba
ustasnov <ustasnov@gmail.com>
2023-02-07 16:05:26 +0700

    Task 3 and 4 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,44 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).reduce((acc, item, index, arr) => {
+    acc += item.age;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+  return result;
 }
+
+console.log(compareArrays([8, 9], [6])); // false, разные значения
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])); // false, разные значения
+console.log(compareArrays([1, 2, 3], [2, 3, 1])); // false, разные индексы, хотя и одинаковые значения
+console.log(compareArrays([8, 1, 2], [8, 1, 2])); // true
+
+const people = [
+  {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+  {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+  {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+  {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+  {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+  {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+  {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+  {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+  {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+  {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+  {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+  {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+  {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+  {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+];
+
+console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
+console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
+console.log(getUsersNamesInAgeRange([], "женский")); // 0
+console.log(getUsersNamesInAgeRange(people, "инопланетянин")); // 0
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,59 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty("marks") || this.marks.length == 0) {
+    return 0;
+  }
+  let result = this.marks.reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+  return Math.round(result);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+
+let student = new Student("Василиса", "женский", 19);
+console.log(student);
+student.setSubject("Algebra");
+console.log(student);
+student.setSubject("Geometry");
+console.log(student);
+student.addMarks(5, 4, 5);
+console.log(student.marks);
+student.addMarks(5, 4, 5);
+console.log(student.marks);
+console.log(student.getAverage());
+student.marks = [3, 4, 5];
+console.log(student.marks);
+console.log(student.getAverage());
+console.log(student);
+student.exclude('прогулы');
+console.log(student);
+student.addMarks(5, 5, 5);
+console.log(student);
+console.log(student.getAverage());
